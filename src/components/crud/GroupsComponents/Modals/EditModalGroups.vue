0bfa48530dfacad09d8a7d15b71fba1d78a97a94
William Joel Chávez López <williamjoel1909@gmail.com>
2025-03-27 03:38:58 -0500

    Validaciones v2

--- src/components/crud/GroupsComponents/Modals/EditModalGroups.vue
+++ src/components/crud/GroupsComponents/Modals/EditModalGroups.vue
@@ -1,4 +1,5 @@
 <script lang="ts" setup>
+import { ref } from 'vue';
 import Dialog from 'primevue/dialog';
 import InputText from 'primevue/inputtext';
 import Button from 'primevue/button';
@@ -14,28 +15,56 @@ const emit = defineEmits<{
   (e: 'update', group: Groups): void;
 }>();
 
+const errors = ref<Record<string, string>>({});
+
+const validateFields = () => {
+  errors.value = {}; // Limpiar errores al validar
+  let isValid = true;
+
+  if (!props.modalItem.nombre?.trim()) {
+    errors.value.nombre = 'El nombre es requerido';
+    isValid = false;
+  }
+  if (!props.modalItem.descripcion?.trim()) {
+    errors.value.descripcion = 'La descripción es requerida';
+    isValid = false;
+  }
+
+  return isValid;
+};
+
 const HandleUpdate = () => {
-  emit('update', props.modalItem);
+  // Limpiar errores antes de hacer el envío
+  errors.value = {};
+  
+  if (validateFields()) {
+    emit('update', props.modalItem);
+  }
 };
 
 const HandleCancel = () => {
+  errors.value = {}; // Limpiar los errores cuando se cancela
   emit('close');
 };
-
 </script>
 
 <template>
   <Dialog v-model:visible="props.showModal" header="Editar Grupo" modal :style="{ width: '30rem' }"
     class="rounded-lg shadow-lg" @update:visible="HandleCancel">
+    
     <!-- Nombre del Grupo -->
     <div class="mb-4">
       <label class="block text-gray-600 text-lg font-medium">Nombre del Grupo</label>
-      <InputText v-model="props.modalItem.nombre" placeholder="Ingresa el nombre del grupo" class="w-full" />
+      <InputText v-model="props.modalItem.nombre" placeholder="Ingresa el nombre del grupo" class="w-full"
+        :class="{ 'p-invalid': errors.nombre }" />
+      <small v-if="errors.nombre" class="text-red-500 text-sm mt-1">{{ errors.nombre }}</small>
     </div>
 
     <div class="mb-4">
       <label class="block text-gray-600 text-lg font-medium">Descripción</label>
-      <InputText v-model="props.modalItem.descripcion" placeholder="Ingresa la descripción del grupo" class="w-full" />
+      <InputText v-model="props.modalItem.descripcion" placeholder="Ingresa la descripción del grupo" class="w-full"
+        :class="{ 'p-invalid': errors.descripcion }" />
+      <small v-if="errors.descripcion" class="text-red-500 text-sm mt-1">{{ errors.descripcion }}</small>
     </div>
 
     <template #footer>
